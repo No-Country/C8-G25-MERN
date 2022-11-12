7d446f8ce128a83e443d2878d9748c4e32100e3a
Gonzalo Martinez <gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.comgit config --global user.email gonzalovam@hotmail.com>
2022-11-12 05:23:41 -0300

    Se conecto la base de datos se creo plantilla test

---
+++ server/src/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+const connectMongo = () => {
+    mongoose.connect('mongodb+srv://app-grm:XJ2r6ixpQcimMf4W@cluster0.c3yq6mn.mongodb.net/?retryWrites=true&w=majority')
+        .then(() => {
+            //don't show the log when it is test
+            if (process.env.NODE_ENV !== "test") {
+                console.log("Connected to %s");
+                console.log("App is running ... \n");
+                console.log("Press CTRL + C to stop the process. \n");
+            }
+        })
+        .catch(err => {
+            console.error("App starting error:", err.message);
+            process.exit(1);
+        });
+}
+
+module.exports = connectMongo
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,27 @@
 const { Router } = require('express');
-
+const modelTest = require('../models/Test')
 
 
 const router = Router();
 
 
 
-router.get('/algo', (req, res) => {
+router.get('/', (req, res) => {
 
     res.send('Hello World!')
 })
+
+
+router.post('/agregar', async (req, res) => {
+    try {
+        const { firstName } = req.query
+        await modelTest.create({ firstName })
+        res.send('Se agrego Correctamente')
+    } catch (err) {
+        res.send(`No anduvo la query ${err}`)
+        console.log('No anduvo la query')
+    }
+})
+
 
 module.exports = router
