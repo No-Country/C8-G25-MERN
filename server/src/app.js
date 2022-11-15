e276bfce51c9d2867766b4361d7af2b556cbf9cf
st1923 <santiagotoloza@hotmail.com>
2022-11-15 09:35:52 -0300

    limpio

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes/index.js')
-const bodyParser = require('body-parser');
 
 ///// Creo la App /////
 const app = express()
