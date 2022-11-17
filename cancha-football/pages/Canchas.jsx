0b69fdc95620234baf47b29f03b7dab535b721cb
Candela <caande.aguirre@gmail.com>
2022-11-17 12:06:46 -0300

    Maquetado y estilos generales

---
+++ cancha-football/pages/Canchas.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+import Navbar from "../components/Navbar/Navbar";
+
+const Canchas = () =>{
+    return (
+        <Navbar/>
+    )
+}
+
+export default Canchas;
--- cancha-football/pages/_app.js
+++ cancha-football/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps }
+    />
 }
 
 export default MyApp
