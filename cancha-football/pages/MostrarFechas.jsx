ddb9aa0c7e07cc0a90a2fe74fa55d411433d57da
st1923 <santiagotoloza@hotmail.com>
2022-11-21 15:17:56 -0300

    prueba

--- cancha-football/pages/MostrarFechas.jsx
+++ cancha-football/pages/MostrarFechas.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import revisarDias from "./Helpers/Dias";
 
 const MostrarFechas = () => {
+  const [dias, setDias] = useState([]);
 
-  
-  
+  const calculardias = () => {
+    for (let i = 0; i < 6; i++) {
+        setDias(revisarDias(i))
+    }
+  };
+  calculardias()
 
   return (
     <div>
       <h1>Selecciona una Fecha</h1>
-        {for (let i = 0)}
-        
     </div>
   );
 };
