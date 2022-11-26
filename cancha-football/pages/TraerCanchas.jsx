f3bea87b53925a9219b144a75fd98142e290055d
st1923 <santiagotoloza@hotmail.com>
2022-11-26 02:01:36 -0300

    a

--- cancha-football/pages/TraerCanchas.jsx
+++ cancha-football/pages/TraerCanchas.jsx
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
+
 import styles from "../styles/Traer.module.css";
 const TraerCanchas = ({ fulvo }) => {
   const [guardarSeleccion, setguardarSeleccion] = useState();
@@ -14,10 +13,10 @@ const TraerCanchas = ({ fulvo }) => {
       </div>
       <div className={styles.Pcolumnas}>
         {fulvo.map((resultado) => (
-          <div className={styles.HColumnas} key={resultado.id} >
+          <div className={styles.HColumnas} key={resultado.id}>
             <h1
               className={styles.h1}
-              onClick={()=>setguardarSeleccion( resultado.id)}
+              onClick={() => setguardarSeleccion(resultado.id)}
             >
               {" "}
               {resultado.nombreCancha}
