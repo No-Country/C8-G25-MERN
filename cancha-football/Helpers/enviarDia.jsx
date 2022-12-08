7a8fc975e734a5c1c48f0bcff62d2c6e08b93a99
st1923 <santiagotoloza@hotmail.com>
2022-12-08 01:16:55 -0300

    avanzado con el horario

---
+++ cancha-football/Helpers/enviarDia.jsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+const enviarDia = (data) => {
+  
+};
+
+export default enviarDia;
--- cancha-football/components/TraerDias/TraerDias.jsx
+++ cancha-football/components/TraerDias/TraerDias.jsx
@@ -4,26 +4,27 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import styles from "../../styles/TraerDias.module.css";
-import enviarDia from "../../Helpers/enviarDia";
-import {es} from 'date-fns/locale/'
-
+import enviarDia from "../../Helpers/enviarDia.jsx";
+import { es } from "date-fns/locale/";
 
 const TraerDias = () => {
   const [value, setValue] = useState(new Date());
-  var options = { year: 'numeric', month: 'long', day: 'numeric' };
-  console.log(
-    value.$d?.toLocaleDateString("es-ES", options)
-  );
-  
-  // enviarDia(datos);
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  const datos = value.$d?.toLocaleDateString("es-ES", options);
 
   const [confirmar, setConfirmar] = useState(false);
+
+  const EnviarFecha = (datos) => {
+    console.log(datos);
+    enviarDia(datos)
+    setConfirmar(true);
+  };
   return (
     <div className={styles.traerDias}>
       <div className={styles.localizerP}>
         <LocalizationProvider
           dateAdapter={AdapterDayjs}
-          adapterLocale="es-mx"
+          locale={es}
           className={styles.color}
         >
           <DatePicker
@@ -38,7 +39,7 @@ const TraerDias = () => {
         </LocalizationProvider>
       </div>
       <div>
-        <button className="btn-confirmar" onClick={() => setConfirmar(true)}>
+        <button className="btn-confirmar" onClick={() => EnviarFecha(datos)}>
           Confirmar
         </button>
       </div>
