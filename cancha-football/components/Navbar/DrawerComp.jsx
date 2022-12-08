7cb8fd6a9c2a22c2da7e6177717ff9911abfea11
GiseCaceres <gcaceres18931@gmail.com>
2022-12-08 09:46:14 -0300

    terminado horas 2

--- cancha-football/components/Navbar/DrawerComp.jsx
+++ cancha-football/components/Navbar/DrawerComp.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import { Drawer,Tooltip, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import { useState } from 'react'
-// import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import linksArray from '../../utils/NavLinks';
+import React from "react";
+import {
+  Drawer,
+  Tooltip,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useState } from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import linksArray from "../../utils/NavLinks";
 
 const DrawerComp = () => {
-
-  const [open, setOpen] = useState(false)  
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-    <Drawer anchor="top" open={open} onClose={()=>setOpen(false)} >
-        <List >
-    
-                 {linksArray.map((linksArray, index) => (
-                
-                  <ListItemButton sx={{width: "0"}}>
-                      <ListItemIcon>
-                          <ListItemText>{linksArray}</ListItemText>
-                      </ListItemIcon>
-                  </ListItemButton>
-                
-                ))}
+      <Drawer anchor="top" open={open} onClose={() => setOpen(false)}>
+        <List>
+          {linksArray.map((linksArray, index) => (
+            <ListItemButton sx={{ width: "0" }}>
+              <ListItemIcon>
+                <ListItemText>{linksArray}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
         </List>
-    </Drawer>
-    <Tooltip title="Add" placement="right">
-    <IconButton sx={{margin:"auto", width:"10vw"}}onClick={()=> setOpen(!open)}>
-        <MenuRoundedIcon sx={{color:"white"}}/>
-    </IconButton>
-    </Tooltip>
+      </Drawer>
+      <Tooltip title="Add" placement="right">
+        <IconButton
+          sx={{ margin: "auto", width: "10vw" }}
+          onClick={() => setOpen(!open)}
+        >
+          <MenuRoundedIcon sx={{ color: "white" }} />
+        </IconButton>
+      </Tooltip>
     </>
-  )
-}
+  );
+};
 
 export default DrawerComp;
