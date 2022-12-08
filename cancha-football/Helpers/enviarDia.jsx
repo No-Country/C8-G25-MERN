bc737062d3c4a37179c30f2abb49e8b80bb7f6e1
st1923 <santiagotoloza@hotmail.com>
2022-12-08 04:05:23 -0300

    funciona ultimos pasos

--- cancha-football/Helpers/enviarDia.jsx
+++ cancha-football/Helpers/enviarDia.jsx
@@ -2,42 +2,60 @@ import horas from "./Horas";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useState } from "react";
-import styles from '../styles/Enviar.module.css'
+import styles from "../styles/Enviar.module.css";
 
 const enviarDia = ({ datos }) => {
-  const [btnvalue, setBtnvalue] = useState(null);
+  const [btnvalue, setBtnvalue] = useState("");
+  const [mostrar, setMostrar] = useState(false);
 
+  const funcionRevisar = (hs) => {
+    setBtnvalue(hs);
+    setMostrar(true);
+  };
   const datosF = datos + " a las " + btnvalue + " hs ";
-  console.log(datosF);
-  console.log(datos);
+
+  
+  const enviar = (datosF) => {  
+    const msjP = "Buenos dias queria alquilar la cancha el dia "
+    const url = `https://api.whatsapp.com/send?phone=542213148680&text=${escape(msjP)}+${escape(datosF)}`
+    console.log(url);
+  }
   return (
     <div className={styles.padreP}>
-      <div>Seleccione un dia </div>
+      <div>Seleccione una hora </div>
 
-      {}
       <ButtonGroup
         variant="contained"
         aria-label="outlined primary button group"
       >
         {horas.map((hs) => (
-          <Button onClick={() => setBtnvalue(hs.horario)} key={hs.id}>
+          <Button onClick={() => funcionRevisar(hs.horario)} key={hs.id}>
             {hs.horario}
           </Button>
         ))}
       </ButtonGroup>
-      
-      <div className={styles.confirmarP}>
-        <p>Usted a selecionado</p> 
-        <p>{btnvalue ? datosF : ""}</p>
-        <ButtonGroup>
-            <Button variant="contained"
-        aria-label="outlined primary button group">Cancelar</Button>
-        <Button variant="contained"
-        aria-label="outlined primary button group">Enviar</Button>
-        </ButtonGroup>
-      </div>
 
-      
+      {mostrar ? (
+        <div className={styles.confirmarP}>
+          <p>Usted a selecionado</p>
+          <p>{btnvalue ? datosF : ""}</p>
+          <ButtonGroup>
+            <Button
+              variant="contained"
+              aria-label="outlined primary button group"
+            >
+              Cancelar
+            </Button>
+            <Button
+              onClick={ ()=> enviar(datosF)}
+              variant="contained"
+              aria-label="outlined primary button group"
+            >
+              Enviar
+            </Button>
+          </ButtonGroup>
+        </div>
+      ) : null}
     </div>
   );
 };
